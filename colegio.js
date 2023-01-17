d10c636ecd5e337175e4dc3f29b54007e4468166
aitorgracia <20aitor02@gmail.com>
2023-01-17 20:23:35 +0100

    17/01/2023_1

--- colegio.js
+++ colegio.js
@@ -1,6 +1,27 @@
 class Colegio
 {
 
+    constructor(nombre,direccion,localidad,nalumnos,naulas){
+
+        this.nombre = nombre
+        this.direccion = direccion
+        this.localidad = localidad
+        this.nalumnos = nalumnos
+        this.naulas = naulas
+
+    }
     
+
+}
+
+class listaColegios
+{
+
+    constructor(colegios)
+    {
+
+        this.colegios = colegios
+
+    }
 
 }
--- main.js
+++ main.js
@@ -1,14 +1,136 @@
+let coles = [];
+
 function princ() {
-    var boton = document.querySelector(".obtenerColegios");
-    boton.addEventListener("click",recibirColegios);
+    var boton1 = document.querySelector(".botonColegio");
+
+    boton1.addEventListener("click",añadirColegios);
+
+    recibirColegios();
+
+}
+
+function añadirColegios() {
+    if (!document.querySelector(".tablaColegios")) {
+        tabla_coles = document.createElement("table")
+        tabla_coles.classList.add("tablaColegios")
+
+        let fila = tabla_coles.insertRow();
+
+        fila.insertCell().innerHTML = "Nombre";
+        fila.insertCell().innerHTML = "Direccion";
+        fila.insertCell().innerHTML = "Localidad";
+        fila.insertCell().innerHTML = "Nº Alumnos";
+        fila.insertCell().innerHTML = "Nº Aulas";
+    }
+    else
+    {
+        tabla_coles = document.querySelector(".tablaColegios")
+    }
+
+    col = new Colegio(
+        document.querySelector(".colnom").value,
+        document.querySelector(".coldir").value,
+        document.querySelector(".colloc").value,
+        document.querySelector(".colnalu").value,
+        document.querySelector(".colnaul").value
+
+
+    )
+    coles.push(col)
+
+    listaColes = new listaColegios(coles);
+    rellenarTablaColegios(listaColes)
+
 }
 
 function recibirColegios() {
 
     var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "colegios.xml", true);
+
+    xhttp.onreadystatechange = function () {
+        
+        if (this.readyState == 4 && this.status == 200) {
+            
+            obtenerListaColegios(JSON.parse(this.responseText))
+
+        }
+
+    }
+
+    xhttp.open("GET", "colegios.json", true);
+
+
     xhttp.send();
+    
 
-    console.log(xhttp.responseXML());
+    
 
+}
+
+function obtenerListaColegios(xhttp) {
+
+    var cole = xhttp.colegios.colegio;
+
+    for (let index = 0; index < cole.length; index++) {
+
+        col = new Colegio(
+            cole[index].nombre,
+            cole[index].direccion,
+            cole[index].localidad,
+            cole[index].nalumnos,
+            cole[index].naulas,
+
+
+        )
+
+        coles.push(col);
+
+        
+    }
+    
+    listaColes = new listaColegios(coles);
+    rellenarTablaColegios(listaColes)
+
+}
+
+function rellenarTablaColegios(listaColes) {
+
+    if (!document.querySelector(".tablaColegios")) {
+        tabla_coles = document.createElement("table")
+        tabla_coles.classList.add("tablaColegios")
+    }
+    else
+    {
+        tabla_coles = document.querySelector(".tablaColegios")
+        tabla_coles.innerHTML = "";
+    }
+    
+    let fila = tabla_coles.insertRow();
+
+    fila.insertCell().innerHTML = "Nombre";
+    fila.insertCell().innerHTML = "Direccion";
+    fila.insertCell().innerHTML = "Localidad";
+    fila.insertCell().innerHTML = "Nº Alumnos";
+    fila.insertCell().innerHTML = "Nº Aulas";
+    fila.insertCell().innerHTML = "Modificaciones";
+    
+
+    for (let i = 0; i < listaColes.colegios.length; i++) {
+        let fila = tabla_coles.insertRow();
+
+        fila.insertCell().innerHTML = listaColes.colegios[i].nombre;
+        fila.insertCell().innerHTML = listaColes.colegios[i].direccion;
+        fila.insertCell().innerHTML = listaColes.colegios[i].localidad;
+        fila.insertCell().innerHTML = listaColes.colegios[i].nalumnos;
+        fila.insertCell().innerHTML = listaColes.colegios[i].naulas;
+
+        mod = document.createElement("button")
+        mod.classList.add= ("ModificarColegios("+i+")");
+        mod.innerHTML = "Modificar este";
+
+        fila.insertCell().appendChild(mod);
+        
+    }
+
+    document.querySelector(".contenedor").appendChild(tabla_coles);
 }
